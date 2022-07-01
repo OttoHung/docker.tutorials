82664f5eb1e9395a5bee3352d900de76ce20009a
Otto Hung <totsi04@gmail.com>
2022-07-01 14:24:09 +1000

    Add displaying current directory to show where is the root folder

--- scripts/pack_greeting_from_project.sh
+++ scripts/pack_greeting_from_project.sh
@@ -1,4 +1,6 @@
 IMG_NAME=docker.tutorials:greeting-from-project
 
+echo "Start to build docker image from: " && pwd
+
 # Build docker image from whole project
 docker build -t ${IMG_NAME} -f Dockerfile_greeting .
--- scripts/pack_whole_project.sh
+++ scripts/pack_whole_project.sh
@@ -1,4 +1,6 @@
 IMG_NAME=docker.tutorials:whole_project
 
+echo "Start to build docker image from: " && pwd
+
 # Build docker image from whole project
 docker build -t ${IMG_NAME} .
