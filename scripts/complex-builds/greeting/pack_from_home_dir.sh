45be2bd669108637dd5a43868433b60a201455a5
Otto Hung <totsi04@gmail.com>
2022-07-07 09:57:49 +1000

    Rename script, pack_by_home to pack_from_home_dir

--- scripts/complex-builds/greeting/pack_from_home_dir.sh
+++ scripts/complex-builds/greeting/pack_from_home_dir.sh
@@ -6,8 +6,9 @@ echo "Start to build docker image from: " && pwd
 # Build docker image at root directory
 # The name of build context must be lowercase
 ## Failed because tidle(~) cannot be recognised by docker build
+## Please use `$HOME`` instead
 docker buildx build \
-    --build-context repo=~/repo/docker.tutorials \
-    --build-context greeting=~/repo/docker.tutorials/workspaces/greeting \
+    --build-context repo=$HOME/repo/docker.tutorials \
+    --build-context greeting=$HOME/repo/docker.tutorials/workspaces/greeting \
     -t ${IMG_NAME} \
     -f ./dockerfiles/greeting .
