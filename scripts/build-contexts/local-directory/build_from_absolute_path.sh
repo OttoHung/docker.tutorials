f04acf68cd81c1d5636dd0d5cd4f718d12d08f61
Otto Hung <totsi04@gmail.com>
2022-07-10 15:16:35 +1000

    Restructure build script for building image from an absolute path

---
+++ scripts/build-contexts/local-directory/build_from_absolute_path.sh
@@ -0,0 +1,16 @@
+IMG_NAME=docker.tutorials:absolute-path
+export DOCKER_BUILDKIT=1
+
+echo "Start to build docker image from: " && pwd
+
+# Build docker image from absolute file path.
+# The name of build context must be lowercase.
+# Secret is used to install npm packages in docker deamon.
+## Failed because tidle(~) cannot be recognised by docker build
+## Please use `$HOME` instead
+docker buildx build \
+    --build-context repo=$HOME/Documents/repos/docker.tutorials \
+    --build-context app=$HOME/Documents/repos/docker.tutorials/workspaces/greeting \
+    --secret id=npm,src=$HOME/.npmrc \
+    -t ${IMG_NAME} \
+    -f ./dockerfiles/build-contexts/local-directory/Dockerfile .
