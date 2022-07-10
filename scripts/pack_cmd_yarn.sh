d5fa11fe8d1d494c48c09b3b709faa8377735cdb
Otto Hung <totsi04@gmail.com>
2022-07-10 19:08:33 +1000

    Refactor script and image for CMD with yarn

--- scripts/pack_cmd_yarn.sh
+++
@@ -1,7 +0,0 @@
-IMG_NAME=docker.tutorials:cmd-yarn
-export DOCKER_BUILDKIT=1
-
-echo "Start to build docker image from: " && pwd
-
-# Build docker image from whole project
-docker build -t ${IMG_NAME} -f Dockerfile_cmd_yarn .
