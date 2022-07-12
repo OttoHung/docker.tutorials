e540b895191e263d798d71ff7f8ee250304def75
Otto Hung <totsi04@gmail.com>
2022-07-12 12:01:52 +1000

    Refator script and image for ENTRYPOINT with yarn

--- scripts/pack_entrypoint_yarn.sh
+++
@@ -1,7 +0,0 @@
-IMG_NAME=docker.tutorials:entrypoint-yarn
-export DOCKER_BUILDKIT=1
-
-echo "Start to build docker image from: " && pwd
-
-# Build docker image from whole project
-docker build -t ${IMG_NAME} -f Dockerfile_entrypoint_yarn .
