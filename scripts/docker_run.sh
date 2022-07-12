9ce0e41510d08545ae6ff5d6c47042c07877cc09
Otto Hung <totsi04@gmail.com>
2022-07-12 17:39:00 +1000

    Add disclaimer

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,3 +1,5 @@
+# This script is a helper to run docker container without extra typing
+# This is not an `on purpose` redundant script for this repo.
 read -p "Please enter the tag: " TAG
 
 docker run -p 5478:5478 docker.tutorials:${TAG}
