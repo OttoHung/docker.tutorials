302d9b4933b040f67f862766e9553b23ff19a4a7
Otto Hung <totsi04@gmail.com>
2022-07-12 12:21:50 +1000

    Refactor script and dockerfile for ENTRYPOINT with sh-c running yarn

--- scripts/pack_entrypoint_yarn_sh.sh
+++
@@ -1,7 +0,0 @@
-IMG_NAME=docker.tutorials:entrypoint-yarn-sh
-export DOCKER_BUILDKIT=1
-
-echo "Start to build docker image from: " && pwd
-
-# Build docker image from whole project
-docker build -t ${IMG_NAME} -f Dockerfile_entrypoint_yarn_sh .
