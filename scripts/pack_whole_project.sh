3baa94c42753fee45d1d4073148aeb12109dda8d
Otto Hung <totsi04@gmail.com>
2022-07-15 14:15:42 +1000

    Remove pack_whole_project.sh because run_at_root.sh has same result

--- scripts/pack_whole_project.sh
+++
@@ -1,9 +0,0 @@
-IMG_NAME=docker.tutorials:whole_project
-export DOCKER_BUILDKIT=1
-
-echo "Start to build docker image from: " && pwd
-
-# Build docker image from whole project
-docker buildx build \
-    --secret id=npm,src=$HOME/.npmrc \
-    -t ${IMG_NAME} .
