0e76ef3efab1b767ff223f39c5e1469024338433
Otto Hung <totsi04@gmail.com>
2022-07-15 14:24:16 +1000

    Add script for building image by context in parent directory

--- scripts/pack_build_context_greeting.sh
+++
@@ -1,8 +0,0 @@
-IMG_NAME=docker.tutorials:greeting
-export DOCKER_BUILDKIT=1
-
-echo "Start to build docker image from: " && pwd
-
-# Build docker image from whole project
-cd dockerfiles
-docker build -t ${IMG_NAME} -f ../Dockerfile_build_context ../workspaces/greeting
