74a707522c77a47c26603a30270bc68ca1111893
Otto Hung <totsi04@gmail.com>
2022-07-15 14:13:39 +1000

    Add script for building image from module folder layer
    - This use case shows how the docker build command works in
      different folder layer.

---
+++ scripts/execution-location/run_at_module.sh
@@ -0,0 +1,15 @@
+IMG_NAME=docker.tutorials:run-at-module-layer
+export DOCKER_BUILDKIT=1
+
+echo "Start to build docker image from: " && pwd
+
+# Build docker image from module under "/workspaces"
+# Secret is used to install npm packages in docker deamon.
+cd workspaces
+cd greeting
+
+docker build \
+        --secret id=npm,src=$HOME/.npmrc \
+        --no-cache \
+        -t ${IMG_NAME} \
+        -f ../../dockerfiles/execution-location/Dockerfile .
--- scripts/pack_greeting_from_project.sh
+++
@@ -1,7 +0,0 @@
-IMG_NAME=docker.tutorials:greeting-from-project
-export DOCKER_BUILDKIT=1
-
-echo "Start to build docker image from: " && pwd
-
-# Build docker image from whole project
-docker build -t ${IMG_NAME} -f Dockerfile_greeting .
