7ef9ed6c0cc937fc787dbe57d25a8f6a6129ad04
Otto Hung <totsi04@gmail.com>
2022-07-16 12:23:18 +1000

    Add script to build image from remote git repo via git@ url

--- scripts/build-path/git/git-at/public/build_from_root.sh
+++ scripts/build-path/git/git-at/public/build_from_root.sh
@@ -1,5 +1,9 @@
-IMG_NAME=docker.tutorials:build-path-git-https-public
+IMG_NAME=docker.tutorials:build-path-git-at-public
 export DOCKER_BUILDKIT=1
+
+## In this case, Buildkit doesn't support `SSH` but `git@`.
+## So that the URL of repository must use slash (/) as the
+## delimiter.
 
 # By using this to build docker image, a `Dockerfile` is 
 # required at root directory.
@@ -7,7 +11,7 @@ REPO=git@github.com/OttoHung/docker.tutorials#main
 
 echo "Start to build docker image from: ${REPO}"
 
-# Build docker image from public git repository via HTTPS
+# Build docker image from public git repository via git@
 #
 # The path of `Dockerfile` is not required because this 
 # approach gathers `Dockerfile` from repository.
